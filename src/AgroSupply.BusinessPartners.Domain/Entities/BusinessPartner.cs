3477f44ac6c4f0f3e5e14b376cbf26c6ee8aba53
nitaBorges <159044207+nitaBorges@users.noreply.github.com>
2026-08-25 16:03:23 -0300

    feat: cria entidade BusinessPartner e testes de domínio

---
+++ src/AgroSupply.BusinessPartners.Domain/Entities/BusinessPartner.cs
@@ -0,0 +1,19 @@
+﻿namespace AgroSupply.BusinessPartners.Domain.Entities;
+
+public class BusinessPartner
+{
+    public BusinessPartner(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException(
+                "O nome do parceiro de negócio é obrigatório.",
+                nameof(name));
+
+        Id = Guid.NewGuid();
+        Name = name;
+    }
+
+    public Guid Id { get; }
+
+    public string Name { get; }
+}
---
+++ tests/AgroSupply.BusinessPartners.Domain.Tests/Entities/BusinessPartnerTests.cs
@@ -0,0 +1,58 @@
+﻿using AgroSupply.BusinessPartners.Domain.Entities;
+
+namespace AgroSupply.BusinessPartners.Domain.Tests.Entities;
+
+public class BusinessPartnerTests
+{
+    [Fact]
+    public void Constructor_ShouldCreateBusinessPartner_WhenNameIsValid()
+    {
+        // Arrange
+        const string name = "Agro Forte Ltda";
+
+        // Act
+        var businessPartner = new BusinessPartner(name);
+
+        // Assert
+        Assert.Equal(name, businessPartner.Name);
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowArgumentException_WhenNameIsEmpty()
+    {
+        // Arrange
+        const string name = "";
+
+        // Act
+        Action action = () => new BusinessPartner(name);
+
+        // Assert
+        Assert.Throws<ArgumentException>(action);
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowArgumentException_WhenNameIsWhiteSpace()
+    {
+        // Arrange
+        const string name = "   ";
+
+        // Act
+        Action action = () => new BusinessPartner(name);
+
+        // Assert
+        Assert.Throws<ArgumentException>(action);
+    }
+
+    [Fact]
+    public void Constructor_ShouldGenerateId_WhenBusinessPartnerIsCreated()
+    {
+        // Arrange
+        const string name = "Agro Forte Ltda";
+
+        // Act
+        var businessPartner = new BusinessPartner(name);
+
+        // Assert
+        Assert.NotEqual(Guid.Empty, businessPartner.Id);
+    }
+}
