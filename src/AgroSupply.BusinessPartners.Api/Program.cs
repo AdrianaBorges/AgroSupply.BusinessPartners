4179f53668991bd9bd851607da2e2a9927349869
nitaBorges <159044207+nitaBorges@users.noreply.github.com>
2026-08-25 21:47:20 -0300

    feat: configura persistencia e migration inicial

--- src/AgroSupply.BusinessPartners.Api/Program.cs
+++ src/AgroSupply.BusinessPartners.Api/Program.cs
@@ -1,11 +1,26 @@
+using AgroSupply.BusinessPartners.Application.Abstractions;
+using AgroSupply.BusinessPartners.Application.UseCases.BusinessPartners;
+using AgroSupply.BusinessPartners.Infrastructure.Persistence;
+using AgroSupply.BusinessPartners.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BusinessPartnersDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IBusinessPartnerRepository, BusinessPartnerRepository>();
+
+builder.Services.AddScoped<CreateBusinessPartnerUseCase>();
+builder.Services.AddScoped<GetBusinessPartnerByIdUseCase>();
 
 var app = builder.Build();
 
---
+++ src/AgroSupply.BusinessPartners.Infrastructure/Migrations/20260826003254_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgroSupply.BusinessPartners.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BusinessPartners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BusinessPartners", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BusinessPartners");
+        }
+    }
+}
