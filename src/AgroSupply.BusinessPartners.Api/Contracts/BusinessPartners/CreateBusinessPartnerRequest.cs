5ae7526a491f5440e9b98b9d3cfddf5a9e0bd3ce
nitaBorges <159044207+nitaBorges@users.noreply.github.com>
2026-08-26 11:37:35 -0300

    feat: expose business partner create and get endpoints

---
+++ src/AgroSupply.BusinessPartners.Api/Contracts/BusinessPartners/CreateBusinessPartnerRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace AgroSupply.BusinessPartners.Api.Contracts.BusinessPartners;
+
+public class CreateBusinessPartnerRequest
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Cpf { get; set; } = string.Empty;
+
+    public DateTime BirthDate { get; set; }
+}
---
+++ src/AgroSupply.BusinessPartners.Api/Controllers/BusinessPartnersController.cs
@@ -0,0 +1,51 @@
+﻿using AgroSupply.BusinessPartners.Application.UseCases.BusinessPartners;
+using AgroSupply.BusinessPartners.Api.Contracts.BusinessPartners;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgroSupply.BusinessPartners.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BusinessPartnersController : ControllerBase
+{
+    private readonly CreateBusinessPartnerUseCase _createBusinessPartnerUseCase;
+    private readonly GetBusinessPartnerByIdUseCase _getBusinessPartnerByIdUseCase;
+
+    public BusinessPartnersController(
+    CreateBusinessPartnerUseCase createBusinessPartnerUseCase,
+    GetBusinessPartnerByIdUseCase getBusinessPartnerByIdUseCase)
+    {
+        _createBusinessPartnerUseCase = createBusinessPartnerUseCase;
+        _getBusinessPartnerByIdUseCase = getBusinessPartnerByIdUseCase;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+    CreateBusinessPartnerRequest request)
+    {
+        var businessPartner = await _createBusinessPartnerUseCase.ExecuteAsync(
+            request.Name,
+            request.Cpf,
+            request.BirthDate);
+
+        return CreatedAtAction(
+            nameof(Create),
+            new { id = businessPartner.Id },
+            businessPartner);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var businessPartner =
+            await _getBusinessPartnerByIdUseCase.ExecuteAsync(id);
+
+        if (businessPartner is null)
+            return NotFound();
+
+        return Ok(businessPartner);
+    }
+
+}
+
+
